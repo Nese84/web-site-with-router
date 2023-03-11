4d3acd9e35f078b196f21b648e67269cf0203dc2
Nese84 <nbserbest84@gmail.com>
2023-03-11 17:08:32 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import AppRouter from "./router/AppRouter";
+import "bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
+import Header from "./components/Header";
 
 
 
 
 function App() {
   return (
-    <>    
+    <>
     <AppRouter/>
     </>
   );
